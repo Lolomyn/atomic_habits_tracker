3132177f993523ee956da2aaec1c52abe1bc2aec
Savelev Maxim <vismanmark@yandex.ru>
2025-08-13 23:25:31 +0300

    Настройка прав доступа

--- atomic_habits/views.py
+++ atomic_habits/views.py
@@ -1,16 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Habit
 from .serializers import HabitSerializer, PublicHabitSerializer
 from .paginators import HabitPaginator
+from .permissions import IsOwner
 
 
 class HabitViewSet(viewsets.ModelViewSet):
     """ViewSet для привычки"""
 
     serializer_class = HabitSerializer
-    queryset = Habit.objects.all()
     pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        """Возвращает только привычки текущего пользователя"""
+        return Habit.objects.filter(user=self.request.user)
 
 
 class PublicHabitViewSet(viewsets.ModelViewSet):
