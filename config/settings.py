2464853c0395cb0c20062ee10bb52c8222e43d45
Savelev Maxim <vismanmark@yandex.ru>
2025-08-17 20:17:22 +0300

    fixes

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("ENGINE"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
