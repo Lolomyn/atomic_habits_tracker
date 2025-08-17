7ad0e02cf361238085bf3bc86095fcad53f9e676
Savelev Maxim <vismanmark@yandex.ru>
2025-08-17 19:57:08 +0300

    fixes

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("POSTGRES_ENGINE"),
-        "NAME": os.getenv("POSTGRES_NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
